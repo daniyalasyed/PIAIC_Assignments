4704c144b8b47e8371954311a4c2e71a9fda4f52
syed-dan <d.a.syed123@gmail.com>
2019-05-27 20:20:20 +0500

    Test Commit of deleting Assignment 2 folder except for 1 file

--- Assignment 02 - 42 Assignments/02_01_Area_of_a_circle_from_radius.py
+++
@@ -1,9 +0,0 @@
-## Write a Python program which accepts the radius of a circle from the user and compute the area
-
-from math import pi
-
-r = float(input ('Please enter radius: '))
-
-area = pi*(r)**2
-
-print('Area of the circle = ', area)
